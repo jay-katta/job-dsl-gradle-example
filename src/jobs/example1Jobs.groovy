488324ed2fa6719c2dd5ec28c0afbd50c99a59ea
Jayavardhan Katta <jay-katta@users.noreply.github.com>
2021-11-23 13:25:19 +0530

    Update example1Jobs.groovy

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -5,18 +5,6 @@ folder(basePath) {
     description 'This example shows basic folder/job creation.'
 }
 
-job("$basePath/gradle-example-build") {
-    scm {
-        github repo
-    }
-    triggers {
-        scm 'H/5 * * * *'
-    }
-    steps {
-        gradle 'assemble'
-    }
-}
-
 job("$basePath/gradle-example-deploy") {
     parameters {
         stringParam 'host'
